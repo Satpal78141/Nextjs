037172809b54537a574461ca83eac70a02a3e6f5
SL <satpal@sookshum-labs.com>
2023-05-19 18:34:32 +0530

    message

---
+++ components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div>
+            <div className='flex mw-100 mt-20'>
+                <img src='https://www.subly.app/spotify.png' className='w-10 float-left' />
+                <img src='https://www.subly.app/dropbox.png' className='w-10 rounded-full absolute right-12 ' />
+            </div>
+            <h1 className='fw-bold"'>All your subscriptions in one place</h1>
+        </div>
+    )
+}
+
+export default Main
---
+++ components/Nav.js
@@ -0,0 +1,32 @@
+import navStyle from '../styles/Nav.module.css';
+import Link from 'next/link';
+const Nav = () => {
+  return (
+    <div className='container m-auto'>  
+    <nav className={navStyle.nav}>
+      <ul>
+        <li className={navStyle.logo_list}>
+            <Link href="/" className={navStyle.logo}><img src="/logo.jpg" />Subly</Link>
+        </li>
+        <li >
+            <Link href="#" className={navStyle.grey}>Features</Link>
+        </li>
+        <li>
+            <Link href="#" className={navStyle.grey}>Pricing</Link>
+        </li>
+        <li >
+            <Link href="#" className={navStyle.grey}>FAQ</Link>
+        </li>
+   <li className={navStyle.sign}>
+    <button>Signup</button>
+   </li>
+   <li className={navStyle.login}> 
+        <Link href="#" className={navStyle.grey}>Login</Link>
+   </li>
+      </ul>
+    </nav>
+    </div>
+  )
+}
+
+export default Nav
